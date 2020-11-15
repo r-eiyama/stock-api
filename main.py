14b6ddc4054b4bbddabcbae5dcda008c7004ef76
yamee-dev <ryo.eiyama@icloud.com>
2020-11-15 13:58:26 +0900

    クエリパラメータを学習

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -14,8 +15,15 @@ async def root():
 
 # 型バリデーション
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id" : item_id}
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 # Enum
 @app.get("/models/{model_name}")
@@ -27,3 +35,10 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+# Queryパラメータ
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
